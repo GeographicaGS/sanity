88147d83fd0be09e38b2a07184e215260edae699
Davide Senatra <d.senatra@gmail.com>
2015-07-02 14:29:27 +0200

    sync

--- www/src/js/View/DashboardView.js
+++ www/src/js/View/DashboardView.js
@@ -34,32 +34,24 @@ App.View.Dashboard = Backbone.View.extend({
     	var _this = this;
     	var sql = new cartodb.SQL({ user: App.config.account});
     	
-    	sql.execute('SELECT '+
-          '(SELECT count(cartodb_id) FROM alasarr.diseases_pox) as total,' +
-          '(SELECT count(cartodb_id) ' +
-          ' FROM alasarr.diseases_pox ' +
-          '  WHERE date_disease>=\'2013-01-01 00:00\' AND date_disease<=\'2013-12-31 23:59:59\') as total_2013,' +
-          '(SELECT count(cartodb_id) ' +
-          '  FROM alasarr.diseases_pox ' +
-          '  WHERE date_disease>=\'2014-01-01 00:00\' AND date_disease<=\'2014-12-31 23:59:59\') as total_2014')
+    	// sql.execute('SELECT '+
+     //      '(SELECT count(cartodb_id) FROM alasarr.diseases_pox) as total' +
+     //      ',(SELECT count(cartodb_id) ' +
+     //      ' FROM alasarr.diseases_pox ' +
+     //      '  WHERE date_disease>=\'2013-01-01 00:00\' AND date_disease<=\'2013-12-31 23:59:59\') as total_2013,' +
+     //      '(SELECT count(cartodb_id) ' +
+     //      '  FROM alasarr.diseases_pox ' +
+     //      '  WHERE date_disease>=\'2014-01-01 00:00\' AND date_disease<=\'2014-12-31 23:59:59\') as total_2014')
+        sql.execute('SELECT count(cartodb_id) as total FROM diseases_pox')
               .done(function(data) {
                   var data = data.rows[0];
                   _this.$el.html(Mustache.render(_this._template,{
                           total : App.formatNumber(data.total),
-                          total_2013 : App.formatNumber(data.total_2013),
-                          total_2014 : App.formatNumber(data.total_2014),
+                          //total_2013 : App.formatNumber(data.total_2013),
+                          //total_2014 : App.formatNumber(data.total_2014),
                           cons: App.Cons
                         }));
-                  //console.log(data)
-                  // $.getJSON(App.config.api_base_url + '/transactions_sum')
-                  //   .done(function(data2) {
-                  //       _this.$el.html(Mustache.render(_this._template,{
-                  //         total_registrations:App.formatNumber(data.total_registrations,0),
-                  //         total_transactions:App.formatNumber(data2.rows[0].n,2),
-                  //         total_twitter:App.formatNumber(data.total_twitter,0),
-                  //         cons: App.Cons
-                  //       }));
-                  //   });
+                  
               })
               .error(function(errors) {
                   throw errors;
